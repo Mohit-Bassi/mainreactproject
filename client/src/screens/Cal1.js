63c781c0e18dc74d5bf0a33b834e62c5096b3afd
Mohit-Bassi <mohitbassi006@gmail.com>
2025-09-18 12:58:20 +0530

    changes

---
+++ client/src/screens/Cal1.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+function Cal1() {
+  const [data, setData] = useState({
+    num1: 0,
+    num2: 0,
+    res: 0,
+  });
+
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: Number(e.target.value) });
+  };
+
+  function sumClick() {
+    setData({ ...data, res: data.num1 + data.num2 });
+  }
+  function minClick() {
+    setData({ ...data, res: data.num1 - data.num2 });
+  }
+  function mulClick() {
+    setData({ ...data, res: data.num1 * data.num2 });
+  }
+  function divClick() {
+    setData({ ...data, res: data.num1 / data.num2 });
+  }
+  function clearClick() {
+    setData({ num1: 0, num2: 0, res: 0 });
+  }
+  return (
+    <>
+      <b> Enter First Number : </b>
+      <input
+        type="number"
+        name="num1"
+        onChange={changeHandler}
+        value={data.num1}
+      />
+      <br />
+      <br />
+      <b> Enter Second Number : </b>
+      <input
+        type="number"
+        name="num2"
+        onChange={changeHandler}
+        value={data.num2}
+      />
+      <br />
+      <br />
+      <b>Result : </b>
+      {data.res}
+      <br />
+      <button className="btn btn-info m-1" onClick={sumClick}>
+        Sum
+      </button>
+      <button className="btn btn-primary m-1" onClick={minClick}>
+        Min
+      </button>
+      <button className="btn btn-success m-1 " onClick={mulClick}>
+        Mul
+      </button>
+      <button className="btn btn-warning m-1" onClick={divClick}>
+        Div
+      </button>
+      <button className="btn btn-danger m-1" onClick={clearClick}>
+        Clear
+      </button>
+    </>
+  );
+}
+
+export default Cal1;
---
+++ server/index.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
